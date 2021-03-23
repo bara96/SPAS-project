84f38e9802406dff26baf361e2a658ae1b7b2332
Matteo Baratella <matteo.baratella96@mail.com>
2021-03-23 21:11:53 +0100

    Updated FlightsController

--- flights/app/Http/Controllers/FlightsController.php
+++ flights/app/Http/Controllers/FlightsController.php
@@ -2,24 +2,161 @@
 
 namespace App\Http\Controllers;
 
-use App\Imports\FlightsImport;
 use App\Models\Flight;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use Maatwebsite\Excel\Facades\Excel;
 
 class FlightsController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function getFlights()
+    public function getFlights(Request $request)
     {
-        $flights = Flight::all([Flight::TRANSACTION_ID, Flight::FLIGHT_DATE, Flight::ORIGIN, Flight::DEST, Flight::CANCELLED])->limit(100);
+        $v = Validator::make($request->all(), [
+            Flight::ID => 'exists:flights|string',
+            Flight::YEAR => "string",
+            Flight::DAY_OF_WEEK => "string",
+            Flight::FLIGHT_DATE => "string",
+            Flight::OP_CARRIER_FL_NUM => "string",
+            Flight::OP_CARRIER_AIRLINE_ID => "string",
+            Flight::ORIGIN_AIRPORT_ID => "string",
+            Flight::ORIGIN => "string",
+            Flight::ORIGIN_CITY_NAME => "string",
+            Flight::ORIGIN_STATE_NM => "string",
+            Flight::DEST_AIRPORT_ID => "string",
+            Flight::DEST => "string",
+            Flight::DEST_CITY_NAME => "string",
+            Flight::DEST_STATE_NM => "string",
+            Flight::DEP_TIME => "string",
+            Flight::DEP_DELAY => "string",
+            Flight::ARR_TIME => "string",
+            Flight::ARR_DELAY => "string",
+            Flight::CANCELLED => "string",
+            Flight::AIR_TIME => "string",
+            'page' => 'integer',
+        ]);
+        if ($v->fails()) {
+            return response()->json($v->messages());
+        }
+
+        $flights = DB::table(Flight::TABLE);
+
+        if(isset($v->validated()[Flight::ID]))
+            $flights = $flights->orWhere(Flight::ID, "=", $v->validated()[Flight::ID]);
+
+        if(isset($v->validated()[Flight::YEAR]))
+            $flights = $flights->orWhere(Flight::YEAR, "=", $v->validated()[Flight::YEAR]);
+
+        if(isset($v->validated()[Flight::DAY_OF_WEEK]))
+            $flights = $flights->orWhere(Flight::DAY_OF_WEEK, "=", $v->validated()[Flight::DAY_OF_WEEK]);
+
+        if(isset($v->validated()[Flight::FLIGHT_DATE]))
+            $flights = $flights->orWhere(Flight::FLIGHT_DATE, "=", $v->validated()[Flight::FLIGHT_DATE]);
+
+        if(isset($v->validated()[Flight::OP_CARRIER_FL_NUM]))
+            $flights = $flights->orWhere(Flight::OP_CARRIER_FL_NUM, "=", $v->validated()[Flight::OP_CARRIER_FL_NUM]);
+
+        if(isset($v->validated()[Flight::OP_CARRIER_FL_NUM]))
+            $flights = $flights->orWhere(Flight::OP_CARRIER_FL_NUM, "=", $v->validated()[Flight::OP_CARRIER_FL_NUM]);
+
+        if(isset($v->validated()[Flight::OP_CARRIER_AIRLINE_ID]))
+            $flights = $flights->orWhere(Flight::OP_CARRIER_AIRLINE_ID, "=", $v->validated()[Flight::OP_CARRIER_AIRLINE_ID]);
+
+        if(isset($v->validated()[Flight::ORIGIN_AIRPORT_ID]))
+            $flights = $flights->orWhere(Flight::ORIGIN_AIRPORT_ID, "=", $v->validated()[Flight::ORIGIN_AIRPORT_ID]);
+
+        if(isset($v->validated()[Flight::ORIGIN]))
+            $flights = $flights->orWhere(Flight::ORIGIN, "=", $v->validated()[Flight::ORIGIN]);
+
+        if(isset($v->validated()[Flight::ORIGIN_CITY_NAME]))
+            $flights = $flights->orWhere(Flight::ORIGIN_CITY_NAME, "=", $v->validated()[Flight::ORIGIN_CITY_NAME]);
+
+        if(isset($v->validated()[Flight::ORIGIN_STATE_NM]))
+            $flights = $flights->orWhere(Flight::ORIGIN_STATE_NM, "=", $v->validated()[Flight::ORIGIN_STATE_NM]);
+
+        if(isset($v->validated()[Flight::DEST_AIRPORT_ID]))
+            $flights = $flights->orWhere(Flight::DEST_AIRPORT_ID, "=", $v->validated()[Flight::DEST_AIRPORT_ID]);
+
+        if(isset($v->validated()[Flight::DEST]))
+            $flights = $flights->orWhere(Flight::DEST, "=", $v->validated()[Flight::DEST]);
+
+        if(isset($v->validated()[Flight::DEST_CITY_NAME]))
+            $flights = $flights->orWhere(Flight::DEST_CITY_NAME, "=", $v->validated()[Flight::DEST_CITY_NAME]);
+
+        if(isset($v->validated()[Flight::DEST_STATE_NM]))
+            $flights = $flights->orWhere(Flight::DEST_STATE_NM, "=", $v->validated()[Flight::DEST_STATE_NM]);
+
+        if(isset($v->validated()[Flight::DEP_TIME]))
+            $flights = $flights->orWhere(Flight::DEP_TIME, "=", $v->validated()[Flight::DEP_TIME]);
+
+        if(isset($v->validated()[Flight::DEP_DELAY]))
+            $flights = $flights->orWhere(Flight::DEP_DELAY, "=", $v->validated()[Flight::DEP_DELAY]);
+
+        if(isset($v->validated()[Flight::ARR_TIME]))
+            $flights = $flights->orWhere(Flight::ARR_TIME, "=", $v->validated()[Flight::ARR_TIME]);
+
+        if(isset($v->validated()[Flight::ARR_DELAY]))
+            $flights = $flights->orWhere(Flight::ARR_DELAY, "=", $v->validated()[Flight::ARR_DELAY]);
+
+        if(isset($v->validated()[Flight::CANCELLED]))
+            $flights = $flights->orWhere(Flight::CANCELLED, "=", $v->validated()[Flight::CANCELLED]);
+
+        if(isset($v->validated()[Flight::AIR_TIME]))
+            $flights = $flights->orWhere(Flight::AIR_TIME, "=", $v->validated()[Flight::AIR_TIME]);
+
+
+        return response()->json($flights->paginate(100), 200);
+    }
+
+    public function endpoint1(Request $request)
+    {
+        $v = Validator::make($request->all(), [
+            Flight::FLIGHT_DATE => 'required|date_format:Y-m-d',
+            Flight::ID => 'required|exists:flights|string',
+            'page' => 'integer',
+        ]);
+        if ($v->fails()) {
+            return response()->json($v->messages());
+        }
+
+        $flights = DB::table(Flight::TABLE)
+            ->where(Flight::ID, "=", $v->validated()[Flight::ID])
+            ->where(Flight::FLIGHT_DATE, "=", $v->validated()[Flight::FLIGHT_DATE])
+            ->get([Flight::ID, Flight::FLIGHT_DATE, Flight::DEP_DELAY, Flight::ARR_DELAY]);
 
         return response()->json($flights, 200);
+    }
+
+    public function endpoint2(Request $request)
+    {
+        $v = Validator::make($request->all(), [
+            'start_date' => 'required|date_format:yyyymmaa',
+            'end_date' => 'required|date_format:yyyymmaa',
+            'delay' => 'required|integer',
+            'page' => 'integer',
+        ]);
+        if ($v->fails()) {
+            return response()->json($v->messages());
+        }
+
+    }
+
+    public function endpoint3(Request $request)
+    {
+        $v = Validator::make($request->all(), [
+            'start_date' => 'required|date_format:yyyymmaa',
+            'end_date' => 'required|date_format:yyyymmaa',
+            'n' => 'required|integer|min:0',
+            'page' => 'integer',
+        ]);
+        if ($v->fails()) {
+            return response()->json($v->messages());
+        }
+
     }
 }
